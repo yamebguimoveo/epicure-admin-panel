76deb3e4278cf2ee644085023ef3ef5c6de6e993
yamebguimoveo <yame@moveo.co.il>
2022-06-08 12:42:48 +0300

    Production

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,23 +32,23 @@ export const Header = () => {
             <img
               className='header-logo'
               alt='header-logo'
-              src='./assets/logo.png'
+              src='/assets/logo.png'
             />
           </a>
 
           <ul className='nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0'>
             <li>
-              <a href='/restaurants' className='nav-link px-2 text-white'>
+              <a href='/admin/restaurants' className='nav-link px-2 text-white'>
                 Restaurants
               </a>
             </li>
             <li>
-              <a href='#' className='nav-link px-2 text-white'>
+              <a href='/admin' className='nav-link px-2 text-white'>
                 Dishes
               </a>
             </li>
             <li>
-              <a href='#' className='nav-link px-2 text-white'>
+              <a href='/admin' className='nav-link px-2 text-white'>
                 Chefs
               </a>
             </li>
